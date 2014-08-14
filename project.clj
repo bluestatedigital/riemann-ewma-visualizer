2ae517e29321a009222384cee08de819b2540baf
Brian Lalor <blalor@bluestatedigital.com>
2014-08-14 09:35:52 -0400

    successfully proxies some data

--- project.clj
+++ project.clj
@@ -6,6 +6,12 @@
     :dependencies [
         [org.clojure/clojure "1.6.0"]
         [http-kit "2.1.16"]
+        [org.clojure/data.json "0.2.5"] ;; https://github.com/clojure/data.json
+        [capacitor "0.2.2"] ;; https://github.com/olauzon/capacitor
+        [compojure "1.1.8"] ;; https://github.com/weavejester/compojure
+        [javax.servlet/servlet-api "2.5"]
+        [ring/ring-devel "1.3.0"]
+        [ring/ring-core "1.3.0"]
     ]
     :main ^:skip-aot influxdb-proxy.core
     :target-path "target/%s"
--- src/influxdb_proxy/core.clj
+++ src/influxdb_proxy/core.clj
@@ -1,7 +1,56 @@
 (ns influxdb-proxy.core
+    (:require [clojure.data.json      :as json]
+              [ring.middleware.reload :as reload]
+              [compojure.route        :as route]
+              [compojure.handler      :as handler]
+              [capacitor.core         :as influx]
+    )
+    (:use [org.httpkit.server :only [run-server]]
+          [compojure.core     :only [defroutes GET POST DELETE ANY context]]
+    )
     (:gen-class))
 
+(defn get-db-series [req]
+    (let [
+        db    (-> req :params :database)
+        query (-> req :params :q)
+        user  (-> req :params :u)
+        pass  (-> req :params :p)
+        
+        client (influx/make-client {
+            :host "influxdb.docker.bsdinternal.com"
+            :port 80
+            :username user
+            :password pass
+            :db db
+        })
+        
+        influx-resp (influx/get-query-req client query)
+        status (:status influx-resp)
+        body (if (= 200 status) (json/read-str (:body influx-resp)))
+    ]
+        {:status status
+         :body (json/write-str body)
+         :headers {"Content-Type" "application/json"}}
+    )
+)
+
+(defroutes all-routes
+    (context "/db/:database" []
+        ;; /db/<database>/series?q=<query>&u=<user>&p=<pass>
+        (GET "/series" [] get-db-series)
+    ) 
+    
+    (route/not-found "I have no idea what you're talking about")
+)
+
 (defn -main
-    "I don't do a whole lot ... yet."
+    "http server entry point"
+    
     [& args]
-    (println "Hello, World!"))
+    
+    (prn "ready")
+    (run-server
+        (reload/wrap-reload (handler/site #'all-routes))
+        {:port 8080})
+)
